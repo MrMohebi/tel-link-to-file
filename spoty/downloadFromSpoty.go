bc7bac9ca5e90aa6f233e7f7cacabedcd9a362d9
MMMohebi <MMMohebi@outlook.com>
2023-03-13 16:45:04 +0330

    push

--- spoty/downloadFromSpoty.go
+++ spoty/downloadFromSpoty.go
@@ -7,8 +7,11 @@ import (
 
 func DownloadAndSave(link string) {
 	folderName := common.RandStr(5)
+	cmdMkdir := exec.Command("mkdir", folderName)
+	_, err := cmdMkdir.Output()
+
 	cmd := exec.Command("spotdl", "download", link, "--output", "'./"+folderName+"/{artist} - {title}.{output-ext}'")
-	_, err := cmd.Output()
+	_, err = cmd.Output()
 	common.IsErr(err)
 
 }
