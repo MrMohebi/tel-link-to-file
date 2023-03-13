d2ec0fee555e2f3e935e31be6f4f2d0dd1ac3b76
MMMohebi <MMMohebi@outlook.com>
2023-03-13 16:24:13 +0330

    push

--- spoty/downloadFromSpoty.go
+++ spoty/downloadFromSpoty.go
@@ -8,7 +8,7 @@ import (
 
 func DownloadAndSave(link string) {
 
-	cmd := exec.Command("spotdl", "download", link)
+	cmd := exec.Command("spotdl", "download", link, "--output", "\"./music/{artist} - {title}.{output-ext}\"")
 	output, err := cmd.Output()
 	common.IsErr(err)
 	println(strings.TrimSuffix(string(output), "\n"))
