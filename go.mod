2f17ec835f11be7f15bed6acd0a25bcf98299512
Amin Mahmudi <mahmudi.moam@gmail.com>
2025-09-24 23:20:38 +0330

    push

--- go.mod
+++ go.mod
@@ -6,12 +6,12 @@ require (
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 	github.com/h2non/filetype v1.1.3
 	github.com/kkdai/youtube/v2 v2.10.1
+	github.com/u2takey/ffmpeg-go v0.5.0
 	gopkg.in/ini.v1 v1.67.0
 	gopkg.in/telebot.v3 v3.2.1
 )
 
 require (
-	github.com/Sorrow446/go-mp4tag v0.0.0-20240130220823-68ce31d53e37 // indirect
 	github.com/aws/aws-sdk-go v1.38.20 // indirect
 	github.com/bitly/go-simplejson v0.5.1 // indirect
 	github.com/dlclark/regexp2 v1.11.0 // indirect
@@ -19,7 +19,6 @@ require (
 	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
 	github.com/google/pprof v0.0.0-20240227163752-401108e1b7e7 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	github.com/u2takey/ffmpeg-go v0.5.0 // indirect
 	github.com/u2takey/go-utils v0.3.1 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/MrMohebi/tel-link-to-file/common"
 	telBot "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/h2non/filetype"
@@ -8,12 +15,6 @@ import (
 	"github.com/kkdai/youtube/v2"
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 	"gopkg.in/ini.v1"
-	"io"
-	"log"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
 )
 
 var isIniInitOnce = false
@@ -22,66 +23,13 @@ var IniData *ini.File
 /////////// nodemon --exec go run main.go --signal SIGTERM
 
 func main() {
-	bot, err := telBot.NewBotAPI(IniGet("", "TOKEN"))
-	if err != nil {
-		common.IsErr(err, true, "Failed to start the bot!")
-	}
+    var link string
 
-	u := telBot.NewUpdate(0)
-	u.Timeout = 60
+    fmt.Println("Enter the link:")
+    fmt.Scan(&link)
 
-	message := bot.GetUpdatesChan(u)
-
-	for message := range message {
-		if message.Message != nil {
-			log.Printf("[%s] %s", message.Message.From.UserName, message.Message.Text)
-
-			chatId := message.Message.Chat.ID
-
-			if message.Message.Text == "/start" {
-				onStart(bot, chatId)
-			} else {
-				text := message.Message.Text
-				isMP3Link := strings.Contains(text, ".mp3")
-				isYTMLink := strings.Contains(text, "music.youtube.com")
-
-				fileName := ""
-
-				var audioFile io.ReadCloser
-
-				if isYTMLink {
-					_, err = bot.Send(telBot.NewMessage(chatId, "Downloading from YTM... :D"))
-					audioFile, fileName = downloadFromYTM(text)
-
-				} else if isMP3Link {
-					_, err = bot.Send(telBot.NewMessage(chatId, "I'm working on it :D"))
-					audioFile, fileName = downloadFromMP3Link(text)
-
-				} else {
-					_, err = bot.Send(telBot.NewMessage(chatId, "Invalid input!"))
-					continue
-				}
-
-				if audioFile == nil {
-					_, err = bot.Send(telBot.NewMessage(chatId, "Invalid input!"))
-					continue
-				}
-
-				file := telBot.FileReader{
-					Name:   fileName,
-					Reader: audioFile,
-				}
-
-				audio := telBot.NewAudio(chatId, file)
-				audio.ReplyToMessageID = message.Message.MessageID
-
-				_, err = bot.Send(audio)
-				if err != nil {
-					common.IsErr(err, false)
-				}
-			}
-		}
-	}
+    x, y := downloadFromMP3Link(link)
+    fmt.Println(x, y)
 }
 
 func IniSetup() {
@@ -107,7 +55,6 @@ func isAudioType(filetype string) bool {
 }
 
 func downloadFromMP3Link(link string) (io.ReadCloser, string) {
-
 	extension := filepath.Base(link)
 	name, _ := strings.CutSuffix(extension, "mp3")
 	extension, _ = strings.CutPrefix(extension, name)
